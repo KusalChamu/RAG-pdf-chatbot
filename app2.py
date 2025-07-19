e76a4cadc1a8843e0f711ff7b1e3f0b3662bc853
KusalChamu <chamuchamp09@gmail.com>
2025-07-19 22:01:25 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyA7oSdx0lQEQQqGjoCuJk0U4Ame8cJH4L4")
+genai.configure(api_key="")
 
 for m in genai.list_models():
     print(m.name, " | ", m.supported_generation_methods)
